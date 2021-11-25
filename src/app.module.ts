8f2a7e779657cacaa786714be441037f96ef310c
Juansecu <juansecoder@hotmail.com>
2021-11-24 20:36:11 -0500

    Configuration for database was set

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
+      host: process.env.DATABASE_HOST,
+      password: process.env.DATABASE_PASSWORD,
+      port: parseInt(process.env.DATABASE_PORT),
+      type: 'mysql',
+      username: process.env.DATABASE_USERNAME,
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
