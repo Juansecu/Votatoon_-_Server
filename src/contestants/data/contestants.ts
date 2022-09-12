0a88e9e32eaf47df92ebf7895ad0bb381652657a
Juansecu <juansecoder@hotmail.com>
2022-09-11 19:57:50 -0500

    FIX - Paths for contestant images were updated

--- src/contestants/data/contestants.ts
+++ src/contestants/data/contestants.ts
@@ -5,232 +5,230 @@ export const contestants: Contestant[] = [
     contestantId: 2,
     name: 'Un Show Más',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/regular_show_2-small.png',
+      'https://votatoon-assets.s3.amazonaws.com/regular_show_2-small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/regular_show_2-large.png'
+      'https://votatoon-assets.s3.amazonaws.com/regular_show_2-large.png'
   },
   {
     contestantId: 3,
     name: 'Mad',
-    smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/mad_3_small.png',
-    largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/mad_3_large.png'
+    smallImagePath: 'https://votatoon-assets.s3.amazonaws.com/mad_3_small.png',
+    largeImagePath: 'https://votatoon-assets.s3.amazonaws.com/mad_3_large.png'
   },
   {
     contestantId: 4,
     name: 'Gumball',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/gumball_4-small.png',
+      'https://votatoon-assets.s3.amazonaws.com/gumball_4-small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/gumball_4-large.png'
+      'https://votatoon-assets.s3.amazonaws.com/gumball_4-large.png'
   },
   {
     contestantId: 6,
     name: 'Johnny Test',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/johnny_test_6-small.png',
+      'https://votatoon-assets.s3.amazonaws.com/johnny_test_6-small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/johnny_test_6-large.png'
+      'https://votatoon-assets.s3.amazonaws.com/johnny_test_6-large.png'
   },
   {
     contestantId: 8,
     name: 'El Chavo',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/el_chavo_8-small.png',
+      'https://votatoon-assets.s3.amazonaws.com/el_chavo_8-small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/el_chavo_8-large.png'
+      'https://votatoon-assets.s3.amazonaws.com/el_chavo_8-large.png'
   },
   {
     contestantId: 9,
     name: 'Hora de Aventura',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/adventure_time_9-small.png',
+      'https://votatoon-assets.s3.amazonaws.com/adventure_time_9-small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/adventure_time_9-large.png'
+      'https://votatoon-assets.s3.amazonaws.com/adventure_time_9-large.png'
   },
   {
     contestantId: 10,
     name: 'La Pantera Rosa',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/pink_panther_&_pals_10-small.png',
+      'https://votatoon-assets.s3.amazonaws.com/pink_panther_&_pals_10-small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/pink_panther_&_pals_10-large.png'
+      'https://votatoon-assets.s3.amazonaws.com/pink_panther_&_pals_10-large.png'
   },
   {
     contestantId: 11,
     name: 'Tom y Jerry',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/tom_&_jerry_11-small.png',
+      'https://votatoon-assets.s3.amazonaws.com/tom_&_jerry_11-small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/tom_&_jerry_11-large.png'
+      'https://votatoon-assets.s3.amazonaws.com/tom_&_jerry_11-large.png'
   },
   {
     contestantId: 14,
     name: 'El Show de los Looney Tunes',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/the_looney_tunes_show_14-small.png',
+      'https://votatoon-assets.s3.amazonaws.com/the_looney_tunes_show_14-small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/the_looney_tunes_show_14-large.png'
+      'https://votatoon-assets.s3.amazonaws.com/the_looney_tunes_show_14-large.png'
   },
   {
     contestantId: 18,
     name: 'Dragoball Z Kai',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/dragonball_z_kai_18-small.png',
+      'https://votatoon-assets.s3.amazonaws.com/dragonball_z_kai_18-small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/dragonball_z_kai_18-large.png'
+      'https://votatoon-assets.s3.amazonaws.com/dragonball_z_kai_18-large.png'
   },
   {
     contestantId: 26,
     name: 'Scooby-Doo',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/scooby-doo_26-small.png',
+      'https://votatoon-assets.s3.amazonaws.com/scooby-doo_26-small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/scooby-doo_26-large.png'
+      'https://votatoon-assets.s3.amazonaws.com/scooby-doo_26-large.png'
   },
   {
     contestantId: 31,
     name: 'Ben 10: Omniverse',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/ben_10%3A_omniverse_31_small.png',
+      'https://votatoon-assets.s3.amazonaws.com/ben_10_omniverse_31_small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/ben_10%3A_omniverse_31_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/ben_10_omniverse_31_large.png'
   },
   {
     contestantId: 33,
     name: 'Las Sombrías Aventuras de Billy y Mandy',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/the_grim_adventures_of_billy_&_mandy_33_small.png',
+      'https://votatoon-assets.s3.amazonaws.com/the_grim_adventures_of_billy_&_mandy_33_small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/the_grim_adventures_of_billy_&_mandy_33_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/the_grim_adventures_of_billy_&_mandy_33_large.png'
   },
   {
     contestantId: 35,
     name: 'Johnny Bravo',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/johnny_bravo_35_small.png',
+      'https://votatoon-assets.s3.amazonaws.com/johnny_bravo_35_small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/johnny_bravo_35_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/johnny_bravo_35_large.png'
   },
   {
     contestantId: 40,
     name: 'El Nuevo Show del Pájaro Loco',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/the_new_woody_woodpecker_show_40_small.png',
+      'https://votatoon-assets.s3.amazonaws.com/the_new_woody_woodpecker_show_40_small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/the_new_woody_woodpecker_show_40_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/the_new_woody_woodpecker_show_40_large.png'
   },
   {
     contestantId: 41,
     name: 'Ben 10: Fuerza Alienigena',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/ben_10%3A_alien_force_41_small.png',
+      'https://votatoon-assets.s3.amazonaws.com/ben_10_alien_force_41_small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/ben_10%3A_alien_force_41_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/ben_10_alien_force_41_large.png'
   },
   {
     contestantId: 43,
     name: 'El Campamento de Lazlo',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/camp_lazlo_43_small.png',
+      'https://votatoon-assets.s3.amazonaws.com/camp_lazlo_43_small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/camp_lazlo_43_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/camp_lazlo_43_large.png'
   },
   {
     contestantId: 44,
     name: 'Level Up',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/level_up_44_small.png',
+      'https://votatoon-assets.s3.amazonaws.com/level_up_44_small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/level_up_44_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/level_up_44_large.png'
   },
   {
     contestantId: 45,
     name: 'Las Chicas Superpoderosas',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/the_powerpuff_girls_45_small.png',
+      'https://votatoon-assets.s3.amazonaws.com/the_powerpuff_girls_45_small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/the_powerpuff_girls_45_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/the_powerpuff_girls_45_large.png'
   },
   {
     contestantId: 48,
     name: 'Young Justice',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/young_justice_48_small.png',
+      'https://votatoon-assets.s3.amazonaws.com/young_justice_48_small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/young_justice_48_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/young_justice_48_large.png'
   },
   {
     contestantId: 51,
     name: 'Detentionaire',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/detentionaire_51_small.png',
+      'https://votatoon-assets.s3.amazonaws.com/detentionaire_51_small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/detentionaire_51_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/detentionaire_51_large.png'
   },
   {
     contestantId: 54,
     name: 'La CQ',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/la_cq_54_small.png',
+      'https://votatoon-assets.s3.amazonaws.com/la_cq_54_small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/la_cq_54_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/la_cq_54_large.png'
   },
   {
     contestantId: 57,
     name: 'Eliot Kid',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/eliot_kid_57_small.png',
+      'https://votatoon-assets.s3.amazonaws.com/eliot_kid_57_small.png',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/eliot_kid_57_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/eliot_kid_57_large.png'
   },
   {
     contestantId: 58,
     name: 'Drama Total: La Vengaza de la Isla',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/total_drama_revenge_of_the_island_58_small.jpg',
+      'https://votatoon-assets.s3.amazonaws.com/total_drama_revenge_of_the_island_58_small.jpg',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/total_drama_revenge_of_the_island_58_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/total_drama_revenge_of_the_island_58_large.png'
   },
   {
     contestantId: 60,
     name: 'Sendokai Champions',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/sendokai_champions_60_small.jpg',
+      'https://votatoon-assets.s3.amazonaws.com/sendokai_champions_60_small.jpg',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/sendokai_champions_60_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/sendokai_champions_60_large.png'
   },
   {
     contestantId: 64,
     name: 'Los Jóvenes Titanes en Acción',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/teen_titans_go!_64_small.jpg',
+      'https://votatoon-assets.s3.amazonaws.com/teen_titans_go!_64_small.jpg',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/teen_titans_go!_64_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/teen_titans_go!_64_large.png'
   },
   {
     contestantId: 66,
     name: 'The Legends of Chima',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/the_legends_of_chima_66_small.jpg',
+      'https://votatoon-assets.s3.amazonaws.com/the_legends_of_chima_66_small.jpg',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/the_legends_of_chima_66_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/the_legends_of_chima_66_large.png'
   },
   {
     contestantId: 67,
     name: 'La Naranja Molesta',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/annoying_orange_67_small.jpg',
+      'https://votatoon-assets.s3.amazonaws.com/annoying_orange_67_small.jpg',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/annoying_orange_67_large.png'
+      'https://votatoon-assets.s3.amazonaws.com/annoying_orange_67_large.png'
   },
   {
     contestantId: 70,
     name: 'Tío Grandpa',
     smallImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/uncle_grandpa_70_small.jpg',
+      'https://votatoon-assets.s3.amazonaws.com/uncle_grandpa_70_small.jpg',
     largeImagePath:
-      'http://nba.cdn.turner.com/toonla/images/votatoon/images/uncle_grandpa_70_small.jpg'
+      'https://votatoon-assets.s3.amazonaws.com/uncle_grandpa_70_small.jpg'
   }
 ];
