3cf91fe2dffdee2111a72e8535f00b02653862a2
Juansecu <juansecoder@hotmail.com>
2022-10-09 03:10:00 -0500

    FIX - Add crossdomain.xml endpoint
    Endpoint for returning crossdomain.xml configuration was implemented

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,10 +4,10 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly _APP_SERVICE: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('crossdomain.xml')
+  getCrossDoman(): string {
+    return this._APP_SERVICE.getCrossDoman();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getCrossDoman(): string {
+    return `<?xml version="1.0" encoding="utf-8" ?>
+      <cross-domain-policy>
+        <allow-access-from domain="*"/>
+      </cross-domain-policy>`;
   }
 }
